ec2f971cd78ea1cf020c1e7b2976372ec1ea8c7e
Karthik k <karthik.chinna7399@gmail.com>
2023-02-27 17:46:19 +0530

    added files

---
+++ src/Components/Form.js
@@ -0,0 +1,23 @@
+import "./Form.css";
+
+ function Form(){
+    return (
+        <>
+        <div className='mainbox2'>
+            <form action='#' method ='post'>
+                <input id='first' type='file' placeholder='No file chosen' accept= 'image/png,image/jpeg,image/jpg'/>
+                <br></br>
+                <input id='second' type='text' placeholder='Author'/>
+                <input id='third' type='text' placeholder='Location'/>
+                <br></br>
+                <input id='fourth' type='text' placeholder='Description'/>
+                <br></br>
+                <button id='post'>Post</button>
+
+            </form>
+            
+        </div>
+        </>
+    )
+}
+export default Form;
